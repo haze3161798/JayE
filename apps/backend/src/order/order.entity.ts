2808f8e66a4ce9fed37e784e90fd18b3c432ed4e
Jay <haze3161798@gmail.com>
2025-01-16 18:47:13 +0800

    reFactor: 新增欄位

--- apps/backend/src/order/order.entity.ts
+++ apps/backend/src/order/order.entity.ts
@@ -4,14 +4,25 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 export class Order {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column({ type: 'datetime' })
   createTime: Date;
+
   @Column({ type: 'datetime' })
   updateTime: Date;
+
   @Column({ type: 'varchar', length: 255 })
   orderStatus: string;
+
   @Column({ type: 'varchar', length: 255 })
   orderNumber: string;
+
   @Column({ type: 'int', comment: '訂單id' })
   productId: number;
+
+  @Column({
+    type: 'int',
+    comment: '訂單狀態 0=未讀 1=已讀 2=進行中 3=出貨中 4=已完成',
+  })
+  status: number;
 }
