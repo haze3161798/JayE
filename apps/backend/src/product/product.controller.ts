092dc38bef6aace44e688d8c7f2ba90c16051de7
Jay <haze3161798@gmail.com>
2025-01-25 17:17:02 +0800

    post api 試作

--- apps/backend/src/product/product.controller.ts
+++ apps/backend/src/product/product.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { ProductService } from './product.service';
-
+import { ProductInsertData } from './types'
 @Controller('product')
 export class ProductController {
   constructor(private productService: ProductService) {}
 
   @Get()
   async getProducts(@Query() query: Record<string, any>) {
-
     const res = await this.productService.getProductBy(query);
     return res;
+  }
+
+  @Post('create')
+  async createProduct(@Body() data: ProductInsertData) {
+    const res = await this.productService.createProduct(data);
+    // return res;
   }
 }
--- apps/backend/src/product/product.service.ts
+++ apps/backend/src/product/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { Product } from './product.entity';
-
+import { ProductInsertData } from './types';
 @Injectable()
 export class ProductService {
   constructor(private dataSource: DataSource) {}
@@ -15,12 +15,22 @@ export class ProductService {
     if (JSON.stringify(data) === '{}') {
       return await this.dataSource.getRepository(Product).find();
     }
+
     const returnData = await this.dataSource.getRepository(Product).findOne({ where: data })
     if (returnData) {
       return returnData
     }
-    console.log(123);
-    
+
     return await this.dataSource.getRepository(Product).find();
+  }
+
+  async createProduct(data: ProductInsertData) {
+    console.log(data);
+    const insertData = {
+      ...data,
+      createTime: new Date(),
+      updateTime: new Date(),
+    }
+    return await this.dataSource.getRepository(Product).save(insertData);
   }
 }
