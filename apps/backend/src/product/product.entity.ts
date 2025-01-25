e95ff615702f79392a733a6c8bd947631088db1f
Jay <haze3161798@gmail.com>
2025-01-25 15:29:54 +0800

    新增 Product schame 內容

--- apps/backend/src/product/product.entity.ts
+++ apps/backend/src/product/product.entity.ts
@@ -19,4 +19,7 @@ export class Product {
 
   @Column({ type: 'text', comment: '商品描述', nullable: true })
   description: string;
+
+  @Column({ type: 'int', default: 9999, comment: '商品價格' })
+  price: number;
 }
