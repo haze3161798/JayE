412678ba6f20e21d0720f2fe1b245de4773fce1f
Jay <haze3161798@gmail.com>
2025-02-11 21:53:48 +0800

    feat:前台建立

---
+++ apps/web/pages/product/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { ProductModel } from "~/types/product";
+
+const { data: products } = useFetch<ProductModel[]>(
+  "http://localhost:8080/product"
+);
+
+onMounted(() => {
+  console.log(JSON.stringify(products.value, null, 2));
+});
+</script>
+
+<template>
+  <div v-for="product in products" :key="product.id">{{ product.name }}</div>
+</template>
